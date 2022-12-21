09aa152fb13356c310e48d985b469fffaa6232df
Andrei Maslau <andrei_maslau@epam.com>
2022-12-21 17:50:09 +0100

    api route changed

--- src/api/api.constants.ts
+++ src/api/api.constants.ts
@@ -1,4 +1,4 @@
-const apiPathProd = 'https://add-cors-to-requests.andrei-maslau.workers.dev';
+const apiPathProd = 'https://trip-organizer-api.up.railway.app';
 const apiPathDev = 'http://localhost:8787';
 
 const isDev = process.env.NODE_ENV === 'development'
